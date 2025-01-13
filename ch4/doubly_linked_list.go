6e73da720e68637af8c8e7b980df2619edc24d7d
Raoul NIYO <vincenira@gmail.com>
2025-01-12 23:21:45 -0500

    initial logic for a doubly linked list

---
+++ ch4/doubly_linked_list.go
@@ -0,0 +1,96 @@
+// Implement a doubly-linked list using generics starting with the code found in structures.go
+package main
+
+import (
+	"fmt"
+)
+
+type node[T comparable] struct {
+	// store the value
+	Data T
+	// store the address of the next linked list
+	next *node[T]
+	// store the addres of the previous
+	prev *node[T]
+}
+
+type list[T comparable] struct {
+	start *node[T]
+}
+
+func (l *list[T]) add(data T) {
+	n := node[T]{
+		Data: data,
+		next: nil,
+		prev: nil,
+	}
+
+	if l.start == nil {
+		l.start = &n
+		return
+	}
+
+	if l.start.next == nil {
+		l.start.next = &n
+		return
+	}
+
+	temp := l.start
+	l.start = l.start.next
+	l.add(data)
+	l.start = temp
+}
+
+func (l *list[T]) delete(data T) {
+	current := l.start
+	previous := l.start
+	found := false
+	for !found && current != nil {
+		if current.Data == data {
+			found = true
+		} else {
+			previous = current
+			current = current.next
+		}
+	}
+	if found {
+		if l.start == nil {
+			l.start = l.start.next
+		} else {
+			previous.next = current.next
+		}
+	}
+}
+
+func (l *list[T]) search(data T) bool {
+	for node := l.start; node != nil; node = node.next {
+		if node.Data == data {
+			return true
+		}
+	}
+	return false
+}
+
+func (l *list[T]) PrintMe() {
+	for node := l.start; node != nil; node = node.next {
+		fmt.Println("*", node.Data)
+	}
+}
+
+func main() {
+	var myList list[int]
+	fmt.Println(myList)
+	myList.add(12)
+	myList.add(9)
+	myList.add(3)
+	myList.add(9)
+	myList.add(1)
+	myList.add(17)
+	myList.PrintMe()
+	value := myList.search(12)
+	valueF := myList.search(8)
+	myList.delete(9)
+	myList.PrintMe()
+	fmt.Println(value)
+	fmt.Println(valueF)
+}
