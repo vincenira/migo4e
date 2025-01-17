b288598dd82ed3a7a8de94472b7e937b866ad265
Raoul NIYO <vincenira@gmail.com>
2025-01-16 22:49:45 -0500

    initial commit for exercises of chapter 5

---
+++ ch5/empty_interfaces.go
@@ -0,0 +1,11 @@
+/*
+Use the empty interface and a function that allows you to differentiate between two dif-
+ferent structures that you have created.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
---
+++ ch5/slices_structures.go
@@ -0,0 +1,14 @@
+/*
+Create a slice of structures using a structure that you created and sort the elements of the
+slice using a field from the structure.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, world!")
+}
