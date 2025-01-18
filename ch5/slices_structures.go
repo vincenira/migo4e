e7e0c6e74db9a2066ef08f285216d47d4f25e968
Raoul NIYO <vincenira@gmail.com>
2025-01-17 23:02:33 -0500

    working version with first exercises for slices_structures

--- ch5/slices_structures.go
+++ ch5/slices_structures.go
@@ -7,8 +7,56 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
+type Contact struct {
+	Lname  string
+	Fname  string
+	Number string
+}
+
+type Contacts []*Contact
+
+func (c Contacts) Len() int      { return len(c) }
+func (c Contacts) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
+
+type ByLname struct{ Contacts }
+
+func (c ByLname) Less(i, j int) bool { return c.Contacts[i].Lname < c.Contacts[j].Lname }
+
+type ByFname struct{ Contacts }
+
+func (c ByFname) Less(i, j int) bool { return c.Contacts[i].Fname < c.Contacts[j].Fname }
+
+type ByNumber struct{ Contacts }
+
+func (n ByNumber) Less(i, j int) bool { return n.Contacts[i].Number < n.Contacts[j].Number }
+func printContacts(cs []*Contact) {
+	for _, c := range cs {
+		fmt.Printf("%-4s %-2s (%s)\n", c.Lname, c.Fname, c.Number)
+	}
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	s := []*Contact{
+		{"tete", "u", "012-130-402920"},
+		{"titi", "i", "012-131-402920"},
+		{"tutu", "a", "012-131-412920"},
+		{"tata", "o", "112-131-412920"},
+		{"toto", "e", "012-131-412921"},
+	}
+	fmt.Println("Sorted by LastName")
+	sort.Sort(ByLname{s})
+	printContacts(s)
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("Sorted by FirstName")
+	sort.Sort(ByFname{s})
+	printContacts(s)
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("Sorted by Number")
+	sort.Sort(ByNumber{s})
+	printContacts(s)
 }
