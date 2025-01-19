126d87b0f01d2ff3d60a19170df0cb941b141267
Raoul NIYO <vincenira@gmail.com>
2025-01-19 17:23:03 -0500

    working version for exercises of chapter 5
    Check with the two structures are of the same type.
    this means that it does not check if the value of the different
    attributs composing a structure is the same to determine if two
    structures are different.

--- ch5/empty_interfaces.go
+++ ch5/empty_interfaces.go
@@ -4,8 +4,45 @@ ferent structures that you have created.
 */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+type Animal struct {
+	Name string
+}
+
+type Fish struct {
+	Name  string
+	Color string
+}
+
+func Differentiate(a, b interface{}) {
+	Atype := reflect.TypeOf(a)
+	Btype := reflect.TypeOf(b)
+	if Atype == Btype {
+		fmt.Println("the two structures are the same")
+	} else {
+		fmt.Println("The two structures are different")
+	}
+
+	fmt.Printf("the first argument type is %s and second argument type is %s\n", Atype.Name(), Btype.Name())
+}
 
 func main() {
-	fmt.Println("vim-go")
+	a := Animal{"girafe"}
+	b := Fish{"whale", "grey"}
+	fmt.Println("let's test if two structures are different or not")
+	fmt.Println("exploring reflect in golang")
+	fmt.Printf("\n\n")
+	Differentiate(a, b)
+	c := Animal{"sheep"}
+	d := Animal{"dog"}
+	fmt.Printf("\n\n")
+	Differentiate(c, d)
+	f := Fish{"whale", "grey"}
+	e := Fish{"whale", "grey"}
+	fmt.Printf("\n\n")
+	Differentiate(f, e)
 }
