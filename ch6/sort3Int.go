9492ce5f2efd7665f65a3f396ce6f148141ad390
Raoul NIYO <vincenira@gmail.com>
2025-01-20 20:33:09 -0500

    chore: add the first exercises of ch06
    the solution doesn't focus on the correcteness of the full comparaison
    however it illustrates the difference of named return and non named
    return

--- ch6/sort3Int.go
+++ ch6/sort3Int.go
@@ -7,6 +7,77 @@ package main
 
 import "fmt"
 
+func nameReturnTriple(a, b, c int) (s, m, M int) {
+	if a < b && a < c {
+		s = a
+		if b < c {
+			M = c
+			m = b
+		} else {
+			M = b
+			m = c
+		}
+	}
+
+	if b < a && b < c {
+		s = b
+		if a < c {
+			M = c
+			m = a
+		} else {
+			M = a
+			m = c
+		}
+	}
+	if c < a && c < a {
+		s = c
+		if b < a {
+			M = a
+			m = b
+		} else {
+			M = b
+			m = a
+		}
+	}
+	return
+}
+
+func unnamed(a, b, c int) (x, y, z int) {
+	if a < b && b < c {
+		return a, b, c
+	}
+	if b < a && a < c {
+		return b, a, c
+	}
+
+	if c < b && b < a {
+		return c, b, a
+	}
+	if b < c && c < a {
+		return b, c, a
+	}
+	return c, a, b
+}
+
 func main() {
-	fmt.Println("vim-go")
+	x, y, z := unnamed(7, 5, 4)
+	a, b, c := nameReturnTriple(7, 5, 4)
+	fmt.Println("unnamed: ", x, y, z)
+	fmt.Println("named: ", a, b, c)
+	x, y, z = unnamed(6, 8, 1)
+	a, b, c = nameReturnTriple(6, 8, 1)
+	fmt.Println("unnamed: ", x, y, z)
+	fmt.Println("named: ", a, b, c)
+	x, y, z = unnamed(1, 2, 3)
+	a, b, c = nameReturnTriple(1, 2, 3)
+	fmt.Println("unnamed: ", x, y, z)
+	fmt.Println("named: ", a, b, c)
+	x, y, z = unnamed(10, 5, 12)
+	a, b, c = nameReturnTriple(10, 5, 12)
+	fmt.Println("unnamed: ", x, y, z)
+	fmt.Println("named: ", a, b, c)
+	x, y, z = unnamed(3, 2, 2)
+	a, b, c = nameReturnTriple(3, 2, 2)
+	fmt.Println("unnamed: ", x, y, z)
+	fmt.Println("named: ", a, b, c)
 }
