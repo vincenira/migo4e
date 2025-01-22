828d20105ff9e051409ec470917d8fb0866d57ac
Raoul NIYO <vincenira@gmail.com>
2025-01-21 22:59:58 -0500

    refactor: change the code to accomodate mariadb

---
+++ ch6/sqlite06mariadb/init_db.sql
@@ -0,0 +1,41 @@
+-- MariaDB script
+
+-- Drop the database if it exists
+-- DROP DATABASE IF EXISTS social_media;
+
+-- Create the database
+-- CREATE DATABASE social_media;
+
+-- Use the database
+-- USE social_media;
+
+-- Table 'UserData'
+-- DROP TABLE IF EXISTS UserData;
+
+-- CREATE TABLE UserData (
+--     id CHAR(36) NOT NULL PRIMARY KEY,
+--     data VARCHAR(160) NOT NULL
+-- );
+
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS social_media;
+
+-- Create the database
+CREATE DATABASE social_media;
+
+-- Use the database
+USE social_media;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Userdata;
+
+CREATE TABLE Users (
+    ID SERIAL,
+    Username VARCHAR(100) PRIMARY KEY
+);
+
+CREATE TABLE Userdata (
+    UserID Int NOT NULL,
+    Name VARCHAR(100),
+    Surname VARCHAR(100),
+    Description VARCHAR(200)
+);
