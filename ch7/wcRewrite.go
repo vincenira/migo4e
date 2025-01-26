cd0142819d5969665767b01a81622ec8b4ee77c3
Raoul NIYO <vincenira@gmail.com>
2025-01-26 18:03:41 -0500

    feat: add printing function wc(1)

--- ch7/wcRewrite.go
+++ ch7/wcRewrite.go
@@ -1,5 +1,5 @@
 /*
-This code groupes 3 exercises of chapter 6.  it will be done in step with commit and comments to illustarte the changes
+This code groupes 3 exercises of chapter 7.  it will be done in step with commit and comments to illustarte the changes
   - use the functionality of byCharacter.go, byLine.go, and byWord.go in order to create a simplified version of the wc(1)
     UNIX utility.
   - Create a full version of the wc(1) UNIX utility, using the viper package to process command line options
@@ -9,6 +9,20 @@ This code groupes 3 exercises of chapter 6.  it will be done in step with commit
 package main
 
 import "fmt"
+
+/*
+printResult takes two arguments
+and then total only if the number of characters of the second arguments is zero
+otherwise it prints both two arguments
+*/
+
+func printResult(total int, fileName string) {
+	if len(fileName) == 0 {
+		fmt.Println(total)
+	} else {
+		fmt.Printf("%d %s", total, fileName)
+	}
+}
 
 func main() {
 	fmt.Println("vim-go")
