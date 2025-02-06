5719e0a35d05c3c6c9009452a2b4bfc34ed48f96
Raoul NIYO <vincenira@gmail.com>
2025-02-05 22:40:05 -0500

    chore: rewrite byline to use bufio.scanner instead of bufio.NewReader

--- ch7/byLineRewrite.go
+++ ch7/byLineRewrite.go
@@ -3,8 +3,40 @@ Go offers bufio.Scanner to read files line by line. Try to rewrite byLine.go usi
 */
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func lineByLine(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	r := bufio.NewScanner(f)
+	for r.Scan() {
+		fmt.Println(r.Text()) // Println will add back the final '\n'
+	}
+	if err := r.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to read data from:", err)
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("vim-go")
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Printf("usage: byLine <file1> [<file2> ...]\n")
+		return
+	}
+
+	for _, file := range args[1:] {
+		err := lineByLine(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
