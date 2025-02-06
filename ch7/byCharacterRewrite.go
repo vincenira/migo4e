91cb568665757194e1e48ad6fc81d29ce18e9900
Raoul NIYO <vincenira@gmail.com>
2025-02-05 22:58:45 -0500

    chore: add the solution of exercise 5 of chapter 7

--- ch7/byCharacterRewrite.go
+++ ch7/byCharacterRewrite.go
@@ -6,8 +6,49 @@ byCharacter.go this way.
 */
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func charByChar(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	for {
+		rChar, n, err := r.ReadRune()
+		if err == io.EOF {
+			if n != 0 {
+				fmt.Println(string(rChar))
+			}
+			break
+		} else if err != nil {
+			fmt.Printf("Error reading file %s", err)
+			return err
+		}
+		fmt.Println(string(rChar))
+
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("vim-go")
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Printf("usage: byCharacter <file1> [<file2> ...]\n")
+		return
+	}
+
+	for _, file := range args[1:] {
+		err := charByChar(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
