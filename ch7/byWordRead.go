fe37b1127ed7ebb5fe930452dfe44768115703ce
Raoul NIYO <vincenira@gmail.com>
2025-02-07 23:26:34 -0500

    chore: solve the last exercise of chapter 7

--- ch7/byWordRead.go
+++ ch7/byWordRead.go
@@ -4,8 +4,39 @@ use Scanner instead
 */
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func wordByWord(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	r := bufio.NewScanner(f)
+	r.Split(bufio.ScanWords)
+	for r.Scan() {
+		fmt.Println(r.Text()) // Println will add back the final '\n'
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("vim-go")
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Printf("usage: byWord <file1> [<file2> ...]\n")
+		return
+	}
+
+	for _, file := range args[1:] {
+		err := wordByWord(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
