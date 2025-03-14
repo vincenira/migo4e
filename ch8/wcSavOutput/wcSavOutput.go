53bbb87467e442f34dee08570d261a2e6616cd87
Raoul NIYO <vincenira@gmail.com>
2025-03-13 23:32:18 -0400

    addin the new directory for wcSavOutput

---
+++ ch8/wcSavOutput/wcSavOutput.go
@@ -0,0 +1,134 @@
+/*
+Try to implement a concurrent version of wc(1) that saves its output to a file.
+Check ligne 360
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"sync"
+)
+
+/*
+To do: adding the logic for channel for reading characters
+*/
+var (
+	wg         sync.WaitGroup
+	totalLines int
+	totalWords int
+	totalChars int
+)
+
+func readFile(path string, storeString chan []string) {
+	defer wg.Done()
+	var readString []string
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	r := bufio.NewReader(f)
+
+	for {
+		// ReadString() returns two values: the string that was read and an error variable.
+		line, err := r.ReadString('\n')
+		if err == io.EOF {
+			if len(line) != 0 {
+				readString = append(readString, line)
+			}
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("error reading file %s", err)
+		}
+		readString = append(readString, line)
+	}
+	storeString <- readString
+}
+
+func wordByWord(tword chan int, readStringChan chan []string) {
+	defer wg.Done()
+
+	total := 0
+	readString := <-readStringChan
+	re := regexp.MustCompile("[^\\s]+")
+	for _, line := range readString {
+		if len(line) != 0 {
+			words := re.FindAllString(line, -1)
+			total += len(words)
+		}
+	}
+	tword <- total
+}
+
+func lineByLine(tline chan int, readStringChan chan []string) {
+	defer wg.Done()
+	readString := <-readStringChan
+	totalL := len(readString)
+	tline <- totalL
+}
+
+func charByChar(tchar chan int, readStringChan chan []string) {
+	defer wg.Done()
+	total := 0
+	totalChars = 0
+	readString := <-readStringChan
+	for _, line := range readString {
+		total += len(string(line))
+	}
+	tchar <- total
+}
+
+func printTotalResult(tline chan int, tword chan int, tchar chan int) {
+	defer wg.Done()
+	totalWords = <-tword
+	totalChars = <-tchar
+	totalLines = <-tline
+
+	if totalLines != 0 {
+		fmt.Printf("%d  ", totalLines)
+	}
+	if totalWords != 0 {
+		fmt.Printf("%d ", totalWords)
+	}
+	if totalChars != 0 {
+		fmt.Printf("%d ", totalChars)
+	}
+	fmt.Printf("total\n")
+}
+
+func printToFile() {
+	fmt.Printf("felow")
+}
+
+func main() {
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Printf("usage: wcShaMem <file1> [<file2> ...]\n")
+		return
+	}
+
+	for _, file := range args[1:] {
+		stringChan := make(chan []string, 1000)
+		wg.Add(1)
+		go readFile(file, stringChan)
+		wg.Wait()
+		wg.Add(3)
+		resultTotalLine := make(chan int, 5)
+		resultTotalChar := make(chan int, 5)
+		resultTotalWord := make(chan int, 5)
+		go lineByLine(resultTotalLine, stringChan)
+		go wordByWord(resultTotalWord, stringChan)
+		go charByChar(resultTotalChar, stringChan)
+		wg.Wait()
+		wg.Add(1) // Add another goroutine to print the total result after all reading goroutines have finished.
+		go printTotalResult(resultTotalLine, resultTotalWord, resultTotalChar)
+		wg.Wait()
+	}
+}
