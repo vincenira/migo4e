561c39789b54ce081ec9078efa5be0e8429517ee
Raoul NIYO <vincenira@gmail.com>
2025-03-21 23:36:43 -0400

    working version of writing to the file
     need to understand how to have to more than one listeners for go channel

--- ch8/wcSavOutput/wcSavOutput.go
+++ ch8/wcSavOutput/wcSavOutput.go
@@ -25,7 +25,6 @@ var (
 )
 
 func readFile(path string, storeString chan []string) {
-	defer wg.Done()
 	var readString []string
 	f, err := os.Open(path)
 	if err != nil {
@@ -52,12 +51,12 @@ func readFile(path string, storeString chan []string) {
 	storeString <- readString
 }
 
-func wordByWord(tword chan int, readStringChan chan []string) {
+func wordByWord(tword chan int, readString []string) {
 	defer wg.Done()
 
 	total := 0
-	readString := <-readStringChan
-	re := regexp.MustCompile("[^\\s]+")
+	// To avoid escape twice, we should use `...` for the regex.MustCompile
+	re := regexp.MustCompile(`[^\s]+`)
 	for _, line := range readString {
 		if len(line) != 0 {
 			words := re.FindAllString(line, -1)
@@ -67,25 +66,23 @@ func wordByWord(tword chan int, readStringChan chan []string) {
 	tword <- total
 }
 
-func lineByLine(tline chan int, readStringChan chan []string) {
+func lineByLine(tline chan int, readString []string) {
 	defer wg.Done()
-	readString := <-readStringChan
 	totalL := len(readString)
 	tline <- totalL
 }
 
-func charByChar(tchar chan int, readStringChan chan []string) {
+func charByChar(tchar chan int, readString []string) {
 	defer wg.Done()
 	total := 0
 	totalChars = 0
-	readString := <-readStringChan
 	for _, line := range readString {
 		total += len(string(line))
 	}
 	tchar <- total
 }
 
-func printTotalResult(tline chan int, tword chan int, tchar chan int) {
+func printTotalResult(tline chan int, tword chan int, tchar chan int, fileName string) {
 	defer wg.Done()
 	totalWords = <-tword
 	totalChars = <-tchar
@@ -100,11 +97,20 @@ func printTotalResult(tline chan int, tword chan int, tchar chan int) {
 	if totalChars != 0 {
 		fmt.Printf("%d ", totalChars)
 	}
-	fmt.Printf("total\n")
+	fmt.Printf("%s\n", fileName)
 }
 
-func createFile(name string) {
-	f, err := os.Create("/tmp/output.txt")
+func isFileExist(fileName string) bool {
+	_, err := os.Stat(fileName)
+	fileExist := false
+	if err == nil {
+		fileExist = true
+	}
+	return fileExist
+}
+
+func createFile(fileName string) {
+	f, err := os.Create(fileName)
 	if err != nil {
 		fmt.Println("error occur in the creation of the file")
 		f.Close()
@@ -115,18 +121,19 @@ func createFile(name string) {
 	f.Close()
 }
 
-func printToFile(tline chan int, tword chan int, tchar chan int) {
+func printToFile(tline chan int, tword chan int, tchar chan int, fileName string) {
 	defer wg.Done()
 	totalLine := <-tline
 	totalWord := <-tword
 	totalChar := <-tchar
-	f, err := os.OpenFile("/tmp/output.txt", os.O_APPEND|os.O_WRONLY, 0o644)
+	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0o644)
 	if err != nil {
 		fmt.Println("error occur")
 		f.Close()
 		return
 	}
 	fmt.Fprintf(f, "%d %d %d total\n", totalLine, totalWord, totalChar)
+	f.Close()
 }
 
 func main() {
@@ -136,21 +143,29 @@ func main() {
 		return
 	}
 
+	// Prepare the saving file
+	fileName := "/tmp/output.txt"
+	if !isFileExist(fileName) {
+		createFile(fileName)
+	}
+
 	for _, file := range args[1:] {
 		stringChan := make(chan []string, 1000)
-		wg.Add(1)
 		go readFile(file, stringChan)
-		wg.Wait()
+		readString := <-stringChan
 		wg.Add(3)
 		resultTotalLine := make(chan int, 5)
 		resultTotalChar := make(chan int, 5)
 		resultTotalWord := make(chan int, 5)
-		go lineByLine(resultTotalLine, stringChan)
-		go wordByWord(resultTotalWord, stringChan)
-		go charByChar(resultTotalChar, stringChan)
+		go lineByLine(resultTotalLine, readString)
+		go wordByWord(resultTotalWord, readString)
+		go charByChar(resultTotalChar, readString)
 		wg.Wait()
-		wg.Add(1) // Add another goroutine to print the total result after all reading goroutines have finished.
-		go printTotalResult(resultTotalLine, resultTotalWord, resultTotalChar)
+		// wg.Add(1) // Add another goroutine to print the total result after all reading goroutines have finished.
+		// go printTotalResult(resultTotalLine, resultTotalWord, resultTotalChar, file)
+		// wg.Wait()
+		wg.Add(1)
+		go printToFile(resultTotalLine, resultTotalWord, resultTotalChar, fileName)
 		wg.Wait()
 	}
 }
