e54f36760280e4804623862c330dfa387014f75d
Raoul NIYO <vincenira@gmail.com>
2025-06-26 23:22:58 -0400

    feat: add client grpc

---
+++ expery/grpc-go-sec/client/client.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.NewClient(":9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+}
