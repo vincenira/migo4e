01be230f2f825ac4176b8dd946018135e5a161f6
Raoul NIYO <vincenira@gmail.com>
2025-06-27 22:53:47 -0400

    refactor: add the logic to call the method in grpc

--- expery/grpc-go-sec/client/client.go
+++ expery/grpc-go-sec/client/client.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"log"
+
+	"grpc-go-sec/chat"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -14,4 +17,12 @@ func main() {
 		log.Fatalf("did not connect: %s", err)
 	}
 	defer conn.Close()
+
+	c := chat.NewChatServiceClient(conn)
+
+	response, err := c.SayHello(context.Background(), &chat.Message{Body: "Hello From Client!"})
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+	log.Printf("Response from server: %s", response.Body)
 }
