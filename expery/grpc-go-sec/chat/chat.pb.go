9f22a5ece01c058e390ba3514c1584d3071817fc
Raoul NIYO <vincenira@gmail.com>
2025-06-28 16:28:44 -0400

    feat: generate the grpc function and datatype helper with protoc
    change that happends into the .pb.go

--- expery/grpc-go-sec/chat/chat.pb.go
+++ expery/grpc-go-sec/chat/chat.pb.go
@@ -65,6 +65,94 @@ func (x *Message) GetBody() string {
 	return ""
 }
 
+type RequestMessage struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	RequestString string                 `protobuf:"bytes,1,opt,name=requestString,proto3" json:"requestString,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *RequestMessage) Reset() {
+	*x = RequestMessage{}
+	mi := &file_chat_proto_msgTypes[1]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *RequestMessage) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*RequestMessage) ProtoMessage() {}
+
+func (x *RequestMessage) ProtoReflect() protoreflect.Message {
+	mi := &file_chat_proto_msgTypes[1]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use RequestMessage.ProtoReflect.Descriptor instead.
+func (*RequestMessage) Descriptor() ([]byte, []int) {
+	return file_chat_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *RequestMessage) GetRequestString() string {
+	if x != nil {
+		return x.RequestString
+	}
+	return ""
+}
+
+type ResponseMessage struct {
+	state          protoimpl.MessageState `protogen:"open.v1"`
+	ResponseString string                 `protobuf:"bytes,1,opt,name=responseString,proto3" json:"responseString,omitempty"`
+	unknownFields  protoimpl.UnknownFields
+	sizeCache      protoimpl.SizeCache
+}
+
+func (x *ResponseMessage) Reset() {
+	*x = ResponseMessage{}
+	mi := &file_chat_proto_msgTypes[2]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *ResponseMessage) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ResponseMessage) ProtoMessage() {}
+
+func (x *ResponseMessage) ProtoReflect() protoreflect.Message {
+	mi := &file_chat_proto_msgTypes[2]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ResponseMessage.ProtoReflect.Descriptor instead.
+func (*ResponseMessage) Descriptor() ([]byte, []int) {
+	return file_chat_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *ResponseMessage) GetResponseString() string {
+	if x != nil {
+		return x.ResponseString
+	}
+	return ""
+}
+
 var File_chat_proto protoreflect.FileDescriptor
 
 const file_chat_proto_rawDesc = "" +
@@ -72,9 +160,15 @@ const file_chat_proto_rawDesc = "" +
 	"\n" +
 	"chat.proto\x12\x04chat\"\x1d\n" +
 	"\aMessage\x12\x12\n" +
-	"\x04body\x18\x01 \x01(\tR\x04body29\n" +
+	"\x04body\x18\x01 \x01(\tR\x04body\"6\n" +
+	"\x0eRequestMessage\x12$\n" +
+	"\rrequestString\x18\x01 \x01(\tR\rrequestString\"9\n" +
+	"\x0fResponseMessage\x12&\n" +
+	"\x0eresponseString\x18\x01 \x01(\tR\x0eresponseString29\n" +
 	"\vChatService\x12*\n" +
-	"\bSayHello\x12\r.chat.Message\x1a\r.chat.Message\"\x00B\tZ\a../chatb\x06proto3"
+	"\bSayHello\x12\r.chat.Message\x1a\r.chat.Message\"\x002J\n" +
+	"\x10BroadcastService\x126\n" +
+	"\x05Greet\x12\x14.chat.RequestMessage\x1a\x15.chat.ResponseMessage\"\x00B\tZ\a../chatb\x06proto3"
 
 var (
 	file_chat_proto_rawDescOnce sync.Once
@@ -88,15 +182,19 @@ func file_chat_proto_rawDescGZIP() []byte {
 	return file_chat_proto_rawDescData
 }
 
-var file_chat_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_chat_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_chat_proto_goTypes = []any{
-	(*Message)(nil), // 0: chat.Message
+	(*Message)(nil),         // 0: chat.Message
+	(*RequestMessage)(nil),  // 1: chat.RequestMessage
+	(*ResponseMessage)(nil), // 2: chat.ResponseMessage
 }
 var file_chat_proto_depIdxs = []int32{
 	0, // 0: chat.ChatService.SayHello:input_type -> chat.Message
-	0, // 1: chat.ChatService.SayHello:output_type -> chat.Message
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	1, // 1: chat.BroadcastService.Greet:input_type -> chat.RequestMessage
+	0, // 2: chat.ChatService.SayHello:output_type -> chat.Message
+	2, // 3: chat.BroadcastService.Greet:output_type -> chat.ResponseMessage
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -113,9 +211,9 @@ func file_chat_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_chat_proto_rawDesc), len(file_chat_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   3,
 			NumExtensions: 0,
-			NumServices:   1,
+			NumServices:   2,
 		},
 		GoTypes:           file_chat_proto_goTypes,
 		DependencyIndexes: file_chat_proto_depIdxs,
